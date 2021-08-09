75dd728135d785af0804d663e2c433301c06bafc
takeru.hashimoto <hstake25@gmail.com>
2021-08-09 22:37:59 +0900

    spa

---
+++ spa/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    key     = "spa.tfstate"
+    encrypt = true
+    region  = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+}
