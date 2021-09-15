3e267a2924b20829e5bca883f4b4a59c4c45d4d9
takeru.hashimoto <hstake25@gmail.com>
2021-09-15 10:13:02 +0900

    ポリシー修正

--- s3_images/data.tf
+++ s3_images/data.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "user" {}
+
 data "aws_route53_zone" "this" {
   name         = var.domain
   private_zone = false
@@ -9,5 +11,6 @@ data "template_file" "s3_policy" {
   vars = {
     origin_access_identity = aws_cloudfront_origin_access_identity.this.id
     bucket_name            = local.bucket_name
+    account_id             = local.account_id
   }
 }
--- s3_images/variables.tf
+++ s3_images/variables.tf
@@ -1,6 +1,7 @@
 locals {
   bucket_name  = "${var.app_name}-s3-images"
   s3_origin_id = "S3-Images-${var.app_name}"
+  account_id   = data.aws_caller_identity.user.account_id
 }
 
 variable "app_name" {}
