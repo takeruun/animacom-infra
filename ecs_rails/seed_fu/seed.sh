bf8dcce75728379b99629aca8fa1a769241ed734
takeru.hashimoto <hstake25@gmail.com>
2021-09-17 09:42:42 +0900

    seed_fu run task

---
+++ ecs_rails/seed_fu/seed.sh
@@ -0,0 +1,14 @@
+output=`aws ecs describe-services --cluster animacom-cluster --services animacom-service | jq ".services[0].networkConfiguration"`
+
+subnets=`echo $output | jq -r '.awsvpcConfiguration.subnets|join(",")'`
+echo $subnets
+securityGroups=`echo $output | jq -r '.awsvpcConfiguration.securityGroups|join(",")'`
+echo $securityGroups
+assignPublicIp=`echo $output | jq -r '.awsvpcConfiguration.assignPublicIp'`
+
+aws ecs run-task \
+  --cluster animacom-cluster \
+  --task-definition animacom-task \
+  --launch-type FARGATE \
+  --overrides file://run_task_seed_fu.json \
+  --network-configuration "awsvpcConfiguration={subnets=[${subnets}],securityGroups=[${securityGroups}],assignPublicIp=${assignPublicIp}}"
